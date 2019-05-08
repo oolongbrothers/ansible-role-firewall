b8297e642dab9892071f938f84dfbcc50a28f899
Alexander Bethke <oolongbrothers@zeibar.net>
2019-05-08 23:34:34 +0200

    Changes according to my taste

---
+++ files/iptables-flush.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+# Completely reset the firewall by removing all rules and chains.
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
